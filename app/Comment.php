2042376af8a4bf7434f70131717eee1d4206227d
Afrijal Dzuhri <ijuldz@gmail.com>
2019-02-12 09:29:02 +0700

    [ADD] comment and post model, controller for add comment in post

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['post_id', 'user_id', 'message'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,11 +5,29 @@
     <div class="row">
       <div class="col-md-8">
 
-        <div class="card">
+        <div class="card mb-3">
           <div class="card-header">{{ $post->title }} | <small>{{ $post->category->name }}</small></div>
 
           <div class="card-body">
             <p>{{ $post->content }}</p>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="card-header">Tambah komentar</div>
+
+          <div class="card-body">
+            <form action="{{ route('post.comment.store', $post) }}" method="POST" class="form-horizontal">
+              {{ csrf_field() }}
+              <div class="form-group">
+                <textarea class="form-control" name="message" id="" cols="30" rows="5" placeholder="Berikan komentar"></textarea>
+              </div>
+
+              <div class="form-group">
+                <button class="btn btn-primary">Komentar</button>
+              </div>
+            </form>
+
           </div>
         </div>
 
